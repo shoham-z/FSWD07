1fe4ba932a26102e1f1149012f1608457a5df6d8
shoham <shozeh123@gmail.com>
2023-07-16 18:46:10 +0300

    deleted some stuff

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,10 @@ import './App.css';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Layout from "./pages/Layout";
 import Login from "./pages/Login";
-import Todos from "./pages/todo/Todos";
 import NoPage from "./pages/NoPage";
-import Posts from "./pages/posts/Posts";
-import Albums from "./pages/albums/Albums";
-import Dashboard from "./pages/Dashboard";
-import Post from "./pages/posts/Post";
-import Album from "./pages/albums/Album";
+
 import {createContext, useState} from "react";
 import Register from "./pages/Register";
-import Settings from "./pages/Settings";
 
 const CurrentUserContext = createContext(null);
 
@@ -32,17 +26,12 @@ function App() {
                 <Routes>
                     <Route index element={<Navigate to='/login/'/>}/>
                     <Route element={<Layout/>}>
-                        <Route path="/users/:username/" element={<Dashboard/>}/>
-                        <Route path="/users/:username/settings" element={<Settings/>}/>
-                        <Route path="/users/:username/todos/" element={<Todos/>}/>
-                        <Route path="/users/:username/posts/" element={<Posts/>}/>
-                        <Route path="/users/:username/posts/:postId" element={<Post/>}/>
-                        <Route path="/users/:username/albums/" element={<Albums/>}/>
-                        <Route path="/users/:username/albums/:albumId" element={<Album/>}/>
+
+                        <Route path="*" element={<NoPage/>}/>
+                        <Route path="login/" element={<Login/>}/>
+                        <Route path="register/" element={<Register/>}/>
                     </Route>
-                    <Route path="*" element={<NoPage/>}/>
-                    <Route path="login/" element={<Login/>}/>
-                    <Route path="register/" element={<Register/>}/>
+
                 </Routes>
             </BrowserRouter>
         </CurrentUserContext.Provider>
